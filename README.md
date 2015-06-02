001f4460990ae0fcddb311a8360bb3ba5676c280
Romain Soufflet <romain@soufflet.io>
2015-06-02 14:25:36 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,27 @@
+Friend Bots
+===========
+
+## Description ##
+
+IRC Bot written in go to simulate the irritating presence of our friends
+
+## Quickstart ##
+
+Install friend-bots with the following command :
+
+```
+go get github.com/gentux/friend-bots
+```
+
+## Creator ##
+
+### Romain Soufflet ###
+
+* [Twitter](http://twitter.com/Romain_Soufflet)
+* [Github](http://github.com/Gentux)
+* [http://romain.soufflet.io](http://romain.soufflet.io)
+
+
+## Legal notices ##
+
+Released under the [MIT License](http://www.opensource.org/licenses/mit-license.php).
---
+++ guilhem.go
@@ -0,0 +1,58 @@
+package main
+
+import "crypto/tls"
+import "fmt"
+import irc "github.com/fluffle/goirc/client"
+import "math/rand"
+
+const channel_name = "#poc_beta_guilhem"
+const channel_nick = "Guilhem"
+
+func main() {
+    cfg := irc.NewConfig(channel_nick)
+    cfg.SSL = true
+    cfg.SSLConfig = &tls.Config{InsecureSkipVerify: true}
+    //cfg.Server = "irc.freenode.net:7000"
+    cfg.Server = "irc.corp.cloudwatt.com:6697"
+    cfg.Pass = "clo45udwatt"
+    cfg.NewNick = func(n string) string { return n + "^" }
+    c := irc.Client(cfg)
+
+    // Add handlers to do things here!
+    // e.g. join a channel on connect.
+    c.HandleFunc("connected", join_channel)
+
+    c.HandleFunc("privmsg", handle_message)
+
+    // And a signal on disconnect
+    quit := make(chan bool)
+    c.HandleFunc("disconnected",
+        func(conn *irc.Conn, line *irc.Line) { quit <- true })
+
+    // Tell client to connect.
+    if err := c.Connect(); err != nil {
+        fmt.Println("Connection error: %s\n", err)
+    } else {
+        fmt.Println("Connected")
+    }
+
+    // Wait for disconnect
+    <-quit
+}
+
+func join_channel(conn *irc.Conn, line *irc.Line) {
+    conn.Join(channel_name)
+}
+
+func handle_message(conn *irc.Conn, line *irc.Line) {
+    //rand.Seed(42)
+    answers := []string{
+        "Mais ! C'est de la merde !",
+        "On doit le réecrire en GO ça !",
+        "C'est moi le plus fort, je suis SURPUISSANT !",
+        "Je parle fort… J'ai du charisme, c'est tout !",
+    }
+    if rand.Intn(10) < 2 {
+        conn.Privmsg(channel_name, answers[rand.Intn(len(answers))])
+    }
+}
