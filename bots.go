511f2d1610fe5d35d32663e93dc3a4e98b2c2cf4
Romain Soufflet <romain@soufflet.io>
2015-06-04 10:24:05 +0200

    Change trigger for displaying messages

--- bots.go
+++ bots.go
@@ -2,29 +2,37 @@ package main
 
 import irc "github.com/fluffle/goirc/client"
 import "math/rand"
+import "regexp"
 
 const channelName = "#poc_beta_test"
 
 func handleMessageGuilhem(conn *irc.Conn, line *irc.Line) {
-	answers := []string{
+	technicalAnswers := []string{
 		"Mais ! C'est de la merde !",
 		"On doit le réecrire en GO ça !",
+		"Mais c'est pas du tout ça le probléme, en fait […]",
+	}
+
+	personalAnswers := []string{
 		"C'est moi le plus fort, je suis SURPUISSANT !",
 		"Je parle fort… J'ai du charisme, c'est tout !",
 		"Yves, Yves, YVES ! Laisse nous parler",
-		"Mais c'est pas du tout ça le probléme, en fait […]",
 		"J'ai un super nom pour le deuxiéme DC ! WATTOO WATTOO",
 		"Attends, y a mon nom sur un t-shirt s'il te plait !",
 	}
-	if rand.Intn(10) < 2 {
-		conn.Privmsg(channelName, answers[rand.Intn(len(answers))])
+
+	openstackRegexp, _ := regexp.Compile("openstack|keystone|deploy|datacenter")
+	technicalPoint := openstackRegexp.FindString(line.Text())
+	if technicalPoint != ""  {
+		conn.Privmsg(channelName, technicalAnswers[rand.Intn(len(technicalAnswers))])
+	} else if rand.Intn(20) < 2 {
+		conn.Privmsg(channelName, personalAnswers[rand.Intn(len(personalAnswers))])
 	}
 }
 
 func main() {
-	// Guilhem
-	go connect(channelName, "GuilhemBot", handleMessageGuilhem)
-
 	// Gentux (for concurency tests)
-	//connect(channelName, "GentuxBot", handleMessageGuilhem)
+	//go connect(channelName, "GentuxBot", handleMessageGuilhem)
+	// Guilhem
+	connect(channelName, "GuilhemBot", handleMessageGuilhem)
 }
